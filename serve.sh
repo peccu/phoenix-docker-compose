487c204898e90aadc65dcf6bd4434674cd9dd7ef
peccu <peccul@gmail.com>
2019-10-27 03:47:02 +0900

    move launch phoenix from command to script

---
+++ serve.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
+cd $SCRIPT_DIR
+if readlink $BASH_SOURCE >/dev/null
+then
+    YML=docker-compose.dev.yml
+    LOGS=./dev-logs.sh
+    WEB=web
+else
+    YML=docker-compose.yml
+    LOGS=./logs.sh
+    WEB=web
+fi
+docker-compose -f $YML exec $WEB bash -c "export COLUMNS=$(tput cols); export LINES=$(tput lines); cd \$APP_NAME; mix phx.server"
