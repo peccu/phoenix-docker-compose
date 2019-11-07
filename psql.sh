0323d66e2b5c4701041484459a64b1235c043dec
peccu <peccul@gmail.com>
2019-11-08 00:20:03 +0900

    fix username for postgres

--- psql.sh
+++ psql.sh
@@ -16,4 +16,4 @@ echo 'describe table	\d [tablename]'
 echo 'cf. https://qiita.com/H-A-L/items/fe8cb0e0ee0041ff3ceb'
 echo 'cf. https://gist.github.com/Kartones/dd3ff5ec5ea238d4c546'
 WINPTY=$(which winpty 2>/dev/null)
-$WINPTY docker-compose -f $YML exec $DB bash -c "export COLUMNS=$(tput cols); export LINES=$(tput lines); exec psql -U todoapp postgres"
+$WINPTY docker-compose -f $YML exec $DB bash -c "export COLUMNS=$(tput cols); export LINES=$(tput lines); exec psql -U postgres postgres"
