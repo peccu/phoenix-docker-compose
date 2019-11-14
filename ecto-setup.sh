dc661d3a14859e55e99658a58c9cb8b91e87def7
peccu <peccul@gmail.com>
2019-11-14 22:32:15 +0900

    update ecto setup script

--- ecto-setup.sh
+++ ecto-setup.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
 cd $SCRIPT_DIR
+# https://gist.github.com/bermi/4125858
+os=${OSTYPE//[0-9.-]*/}
+case "$os" in
+    darwin|linux)
+        export COMPOSE_FILE_DELIMITOR=":"
+        ;;
+    msys)
+        export COMPOSE_FILE_DELIMITOR=";"
+        ;;
+    *)
+        echo "Unknown Operating system $OSTYPE"
+        exit 1
+esac
+
 if readlink $BASH_SOURCE >/dev/null
 then
-    YML=docker-compose.dev.yml
+    export COMPOSE_FILE=docker-compose.dev.yml
     LOGS=./dev-logs.sh
     WEB=web.dev
     MIX_ENV=dev
 else
     # mix does not in prod container
-    YML=docker-compose.dev.yml
+    export COMPOSE_FILE="docker-compose.yml${COMPOSE_FILE_DELIMITOR}docker-compose.dev.yml${COMPOSE_FILE_DELIMITOR}docker-compose.mix-prod.yml"
     WEB=web.dev
     MIX_ENV=prod
     LOGS=./logs.sh
 fi
-docker-compose -f $YML exec $WEB bash -c "export COLUMNS=$(tput cols); export LINES=$(tput lines); cd \$APP_NAME; MIX_ENV=$MIX_ENV mix ecto.create && MIX_ENV=$MIX_ENV mix ecto.migrate"
+docker-compose exec $WEB bash -c "export COLUMNS=$(tput cols); export LINES=$(tput lines); cd \$THIS_APP_NAME; MIX_ENV=$MIX_ENV mix ecto.setup"
