f9950ec9eae9474ec69e5a5521135c3d02086baf
peccu <peccul@gmail.com>
2019-11-21 23:54:27 +0900

    fix serve script

--- serve.sh
+++ serve.sh
@@ -2,9 +2,9 @@
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
 if readlink $BASH_SOURCE >/dev/null
 then
-    WEB=web.dev
-else
     WEB=web
+else
+    WEB=web.dev
 fi
 cd $SCRIPT_DIR
 docker-compose exec $WEB bash -c "export COLUMNS=$(tput cols); export LINES=$(tput lines); cd \$THIS_APP_NAME; iex -S mix phx.server"
